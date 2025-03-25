9126f746f8f37fa3f684a5e9c52641a8ab375bf5
Eduarda-07 <duda.garbulha@gmail.com>
2025-03-24 22:43:32 -0300

    segunda tela ok

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ async function pesquisarContinente (continente){
     return data
 }
 
-function criarCard (link){
-    const galeria = document.getElementById('galeria')
+function criarCard (link, galeria){
+    // const galeria = document.getElementById('galeria')
     const card = document.createElement('div')
     card.classList.add('cards')
     const novaImg = document.createElement('img')
@@ -23,20 +23,35 @@ function criarCard (link){
     card.appendChild(novoNome)
 
     galeria.appendChild(card)
-
+  
 }
 
 async function preencherFotos (){
     const continente = document.getElementById('continente').value
     const fotos = await pesquisarContinente(continente)
+    const tela2 = document.getElementById('tela2')
     const galeria = document.getElementById('galeria')
 
+    tela2.replaceChildren('')
     galeria.replaceChildren('')
-    fotos.forEach(criarCard)
-    
-    console.log (fotos)
-    
+   
+    fotos.forEach((foto) => criarCard(foto, galeria))
+    tela2.appendChild(galeria)
+    tela2.style.display = 'block'
 
+    const h1_modificado = document.querySelector('.h1_original')
+    const input = document.getElementById('continente');
+    const button = document.getElementById('buscar');
+
+    h1_modificado.classList.remove('h1_original');
+    h1_modificado.classList.add('h1_modificado');
+    input.classList.remove('pesquisar_original');
+    input.classList.add('pesquisar_modificado');
+    button.classList.remove('buscar_original');
+    button.classList.add('buscar_modificado');
+
+
+    console.log (fotos) 
 }
 
 document.getElementById('buscar')
